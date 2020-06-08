4bc50386078b3264998169f85bd0b3a742266399
Barri <info@bdesignstudio.co.uk>
2020-06-08 19:14:39 +0100

    delete movie function

--- app.js
+++ app.js
@@ -42,14 +42,21 @@ UI.prototype.showAlert = function (message, className) {
   const container = document.querySelector('.container');
   // -16 Get form
   const form = document.querySelector('#movie-form');
-  //-16 Insert alert
+  //-16 Insert alert-- first arg is what we want to insert --second arg is where we want to insert before
   container.insertBefore(div, form);
   //-16 Timeout after secs
   setTimeout(function () {
     document.querySelector('.alert').remove();
   }, 2000);
 }
+//-19 making delete prototype for UI
 
+UI.prototype.deleteMovie = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+
+}
 
 //-13 Clear fields prototype
 UI.prototype.clearFields = function () {
@@ -60,7 +67,7 @@ UI.prototype.clearFields = function () {
 
 
 
-//-3 Event Listeners
+//-3 Event Listeners for add movie
 document.getElementById('movie-form').addEventListener('submit', function (e) {
 
   const title = document.getElementById('title').value,
@@ -83,13 +90,21 @@ document.getElementById('movie-form').addEventListener('submit', function (e) {
     ui.addMovieToList(movie);
     //-17 Show success
     ui.showAlert('Movie added!', 'success');
-
-
     //-12 Clear the field after adding a movie
     ui.clearFields();
-
-
   }
   // we put step 6 and 12 to validation section
   e.preventDefault();
-}); 
+});
+
+// -18 Event listener for delete
+document.getElementById('movie-list').addEventListener('click', function (e) {
+
+  //-20 instantiate ui
+  const ui = new UI();
+  ui.deleteMovie(e.target);
+  //-21 Show alert for removing
+
+ui.showAlert('Movie has been removed' , 'success');
+  e.preventDefault();
+});
