3447c67e3c884eed94d9e7956cdffa8c2c57239d
Barri <info@bdesignstudio.co.uk>
2020-06-08 19:22:28 +0100

    Set up ES6 classes

---
+++ appES6.js
@@ -0,0 +1,23 @@
+// now we make classes instead of constructors
+
+
+//Class movie
+class Movie{
+  constructor(title, director, imdb){
+    this.title = title;
+    this.director = director;
+    this.imdb = imdb;
+  
+  }
+}
+
+//class UI we pt the methods in there
+class UI {
+  addMovieToList(){}
+
+  showAlert(){}
+
+  deleteMovie(){}
+
+  clearFiled(){}
+}
